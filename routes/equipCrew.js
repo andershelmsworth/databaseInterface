638003e97170851d3f45862a33af92040f8e3914
andershelmsworth <andy.helmsworth@gmail.com>
2020-10-29 22:29:17 -0700

    missing files

---
+++ routes/equipCrew.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/equipCrew', function(req, res, next) {
+
+	/*
+		Server processing code, e.g. DB calls, goes here
+	*/
+	const myUsers = [{}, {}, {}];
+
+	for (let i = 0; i < 3; i++) {
+		myUsers[i].rid = i;
+		myUsers[i].eid = "hello " + i;
+		myUsers[i].cid = "hello" + i + i;
+	}
+
+	res.render('pages/equipCrew', {
+		myUsers: myUsers
+	});
+});
+
+module.exports = router;
---
+++ routes/phaseCrew.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/phaseCrew', function(req, res, next) {
+
+	/*
+		Server processing code, e.g. DB calls, goes here
+	*/
+	const myUsers = [{}, {}, {}];
+
+	for (let i = 0; i < 3; i++) {
+		myUsers[i].rid = i;
+		myUsers[i].pid = "hello " + i;
+		myUsers[i].cid = "hello" + i + i;
+	}
+
+	res.render('pages/phaseCrew', {
+		myUsers: myUsers
+	});
+});
+
+module.exports = router;

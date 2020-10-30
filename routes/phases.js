6b506c61e6415c50e8d46a763f4f9be55879b942
andershelmsworth <andy.helmsworth@gmail.com>
2020-10-29 18:08:32 -0700

    phases and crews

---
+++ routes/phases.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/phases', function(req, res, next) {
+
+	/*
+		Server processing code, e.g. DB calls, goes here
+	*/
+	const myUsers = [{}, {}, {}];
+	for (let i = 0; i < 3; i++) {
+		myUsers[i].pid = i;
+		myUsers[i].pname = "hello " + i;
+		myUsers[i].jid = i + i;
+	}
+	res.users = myUsers;
+	res.render('pages/Phases', {
+		myUsers: myUsers
+	});
+});
+
+module.exports = router;

9a5527ff58923cfe6d6aa28c9eb9946e6183719b
andershelmsworth <andy.helmsworth@gmail.com>
2020-11-21 18:05:58 -0800

    dropdown working

--- routes/phaseCrew.js
+++ routes/phaseCrew.js
@@ -3,7 +3,18 @@ var router = express.Router();
 const getAllQuery = 'SELECT * FROM `Phase_crew`';
 const mysql = require('../dbcon.js');
 
+const getCrewsQuery = 'SELECT `Crews`.`crew_id`, `Crews`.`crew_name` FROM `Crews`;';
+const getPhasesQuery = 'SELECT `Phases`.`phase_id`, `Phases`.`phase_name` FROM `Phases`;';
+
 function getAllData(res) {
+	let context = {};
+	let pcRows;
+	let crewRows;
+	let phaseRows;
+	let initQueryWorked = false;
+	let secondQueryWorked = false;
+	let errorEncountered = false;
+
 	mysql.query(getAllQuery, (err, rows, fields) => {
 		if (err) {
 			next(err);
@@ -13,12 +24,42 @@ function getAllData(res) {
 			//res.json({ rows: rows });
 			let context = {};
 			context = JSON.stringify(rows);
+			initQueryWorked = true;
+			pcRows = rows;
 
-			res.render('pages/phaseCrew', {
-				results: rows
-			});
+			if (initQueryWorked) {
+				mysql.query(getCrewsQuery, (err, srows, fields) => {
+					if (err) {
+						next(err);
+						return;
+					}
+					else {
+						secondQueryWorked = true;
+						crewRows = srows;
+
+						if (secondQueryWorked) {
+							mysql.query(getPhasesQuery, (err, trows, fields) => {
+								if (err) {
+									next(err);
+									return;
+								}
+								else {
+									res.render('pages/phaseCrew', {
+										results: pcRows,
+										pidSelections: trows,
+										cidSelections: crewRows
+									});
+
+								}
+							});
+						}
+					}
+				});
+			}
 		}
 	});
+
+	
 };
 
 router.get('/phaseCrew', function (req, res, next) {
